4ab2752980f18a715393633686d5fdb4aff60b5e
Mohammed Ammar Ammar <m.ammar4web@gmail.com>
2023-07-27 14:43:01 +0300

    Get/Update Profile

--- routes/auth/auth.php
+++ routes/auth/auth.php
@@ -17,4 +17,7 @@ use \App\Http\Controllers\Api\V1\Auth;
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
 
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [Auth\ProfileController::class, 'show']);
+    Route::put('profile', [Auth\ProfileController::class, 'update']);
+});
